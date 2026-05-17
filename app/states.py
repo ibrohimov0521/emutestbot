4c72f53883e50bc234fafd726d4b19299155ee20
Javokhir Ibrokhimov <javohiribrohimov0521@gmail.com>
2026-05-17 19:47:06 +0500

    Make add_user command interactive: app/states.py

--- app/states.py
+++ app/states.py
@@ -3,3 +3,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 class TestStates(StatesGroup):
     answering = State()
+
+
+class AdminStates(StatesGroup):
+    waiting_add_user_id = State()
+    waiting_block_user_id = State()
+    waiting_unblock_user_id = State()
+    waiting_user_stats_id = State()
